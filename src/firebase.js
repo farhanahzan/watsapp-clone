2c5cbcac9d310e71aff3847297291e67c8b1069a
farhanahzan <41515998+farhanahzan@users.noreply.github.com>
2022-11-29 18:22:44 +0530

    chat top

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABOqNaNbsQWN82M2_vxVNyCS-ehBrBX58",
+  authDomain: "watsapp-clone-60f2d.firebaseapp.com",
+  projectId: "watsapp-clone-60f2d",
+  storageBucket: "watsapp-clone-60f2d.appspot.com",
+  messagingSenderId: "223330330626",
+  appId: "1:223330330626:web:070da3d9450faae9e19f1b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
